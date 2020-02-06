c3f6578151d4bf4f249f77f5764eb0817176ea95
Fabrizio <Fabrizio@Carloss-Mac-Pro.local>
2020-02-06 13:50:41 -0500

    Estilo de Interfaz terminado

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 
 prueba = [2,2,2,2];
 
+styleMode = 0;
 
 function crearTabla() {
     $("#table_div tr").remove();
     jQuery.each(prueba, function(index, value) {
         $("#table_div").append("<tr><td>" + value + "</td></tr>");
     });
+}
+
+function toogleNightMode() {
+    if (styleMode == 0) {
+        $( "body" ).css( "background-color", "#fff" );
+        $( "body" ).css( "color", "black" );
+        $( ".table" ).css( "color", "black" );
+    }
+    else {
+        $( "body" ).css( "background-color", "#343a40" );
+        $( "body" ).css( "color", "#fff" );
+        $( ".table" ).css( "color", "#fff" );
+    }
+    styleMode = !styleMode;
 }
 
